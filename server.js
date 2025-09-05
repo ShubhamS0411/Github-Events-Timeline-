ad41ac4cc27f5331f7aa67891f480f4523a9d336
ShubhamS0411 <shubhsm0411@gmail.com>
2025-09-05 11:51:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,4 +19,8 @@ cronjob.schedule("* * * * *", () => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
