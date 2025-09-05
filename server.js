dc6188f95227afb22666d658c6fda4872a994f21
ShubhamS0411 <shubhsm0411@gmail.com>
2025-09-05 12:01:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import sendUpdates from "./src/server/endpoints/sendUpdates.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://github-events-timeline.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(router);
 
